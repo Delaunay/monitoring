95e6e7e9ef91f4259253b51c31199af427298234
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2019-05-02 15:59:23 -0400

    update the analyze script to reflect the change in monitoring

--- monitoring/analyze.py
+++ monitoring/analyze.py
@@ -1,5 +1,8 @@
 from benchutils.statstream import StatStream
 from collections import defaultdict
+from pymongo import MongoClient
+import json
+
 
 gpu_metrics = [
     'memory_used [MiB]',
@@ -8,8 +11,31 @@ gpu_metrics = [
     'memory_total [MiB]'
 ]
 
+cgroup_metrics = [
+    'memory_limit_in_bytes',
+    'memory_usage_in_bytes'
+    # 'devices_allow',
+]
+
+system_metrics = [
+    "cpu%",
+    "cpu_user",
+    "cpu_sys",
+    "cpu_child_user",
+    "cpu_child_sys",
+    "user",
+    "iowait",
+    "system",
+    "mem_rss",
+    "mem_vms",
+    "mem_uss"
+]
+
 
 def acc_gpu(report, key, value):
+    if value is None:
+        return
+
     gpu_count = len(value)
     report['gpu_count'].update(gpu_count)
 
@@ -17,21 +43,73 @@ def acc_gpu(report, key, value):
         for k in gpu_metrics:
             v = gpu.get(k)
             if v:
-                report[k].update(float(v))
+                report[f'gpu_{k}'].update(float(v))
 
 
 def default_acc(report, key, value):
+    if value is None:
+        return
+
     acc = report[key]
     value = float(value)
     acc.update(value)
 
 
+def acc_children(report, key, value):
+    """
+    "children": {
+        "111107": "vi",
+        "107733": "python",
+        "65719": "python",
+        "59794": "python"
+      },
+    """
+    report["children_count"].update(len(value))
+
+
+def acc_system(report, key, value):
+    for metric in system_metrics:
+        val = value.get(metric)
+        if val:
+            report[f'system_{metric}'].update(float(val))
+
+
+def acc_cgroup(report, key, value):
+    """
+    "cgroup":{
+        "cpuset_effective_cpus": {
+          "total_requested": 4,
+          "cpu_set": "4-7"
+        },
+        "cpuacct_usage_percpu": {
+          "4": "24953153148",
+          "5": "32997757400",
+          "6": "25034003872",
+          "7": "29122557948"
+        },
+        "devices_allow": "",
+        "memory_usage_in_bytes": "2064384000",
+        "memory_limit_in_bytes": "8589934592"
+      }
+    """
+
+    val = float(value['cpuset_effective_cpus']['total_requested'])
+    report['requested_cpu_count'].update(val)
+
+    val = value['cpuacct_usage_percpu']
+    for k, usage in val.items():
+        report['cpuacct_usage'].update(float(usage))
+
+    for metric in cgroup_metrics:
+        val = float(value[metric])
+        report[f'cgroup_{metric}'].update(val)
+
+
 metrics = {
     'gpus': acc_gpu,
-    'VSZ': default_acc,
-    'RSS': default_acc,
-    '%MEM': default_acc,
-    '%CPU': default_acc
+    'children': acc_children,
+    'cgroup': acc_cgroup,
+    'system': acc_system
 }
 
 
@@ -40,10 +118,13 @@ def usage_per_user(observations):
     use_util = []
 
     for obs in observations:
-        username = obs['USER']
+        obs['_id'] = str(obs['_id'])
+        # print(json.dumps(obs, indent=2))
+
+        username = obs['user']
 
         user_report = defaultdict(lambda: StatStream(drop_first_obs=0))
-        user_report['USER'] = username
+        user_report['user'] = username
 
         if username in user_util_dict:
             user_report = user_util_dict[username]
@@ -63,7 +144,7 @@ def implode_statstream(usage_report):
     final_report = []
 
     for user_usage in usage_report:
-        user_report = defaultdict(lambda: StatStream(drop_first_obs=0))
+        user_report = defaultdict(lambda: 0)
 
         for k, v in user_usage.items():
             if isinstance(v, StatStream):
@@ -83,6 +164,9 @@ def implode_statstream(usage_report):
 
 
 def to_csv(report):
+    if len(report) <= 0:
+        return [], []
+
     rows = []
     header = list(report[0].keys())
 
@@ -96,7 +180,7 @@ def to_csv(report):
 
 
 if __name__ == '__main__':
-    from pymongo import MongoClient
+
     import pandas as pd
     import argparse
 
@@ -117,12 +201,32 @@ if __name__ == '__main__':
 
     header, data = to_csv(usage_report)
 
-    for line in data:
-        print(line)
-
     df = pd.DataFrame(data, columns=header)
+    df.to_csv('full_report.csv', index=False)
+
+    selected_cols = [
+        'user',
+        'avg_gpu_count', 'avg_gpu_memory_used [MiB]', 'avg_gpu_utilization_memory [%]', 'avg_gpu_memory_total [MiB]',
+        'avg_children_count', 'avg_requested_cpu_count',    # 'avg_cpuacct_usage',
+        'avg_cpuacct_usage_s',
+        # 'avg_cgroup_memory_limit_in_bytes', 'avg_cgroup_memory_usage_in_bytes',
+        'avg_system_cpu_child_user', 'avg_system_cpu_child_sys',
+        'avg_cgroup_memory_limit_in_mib', 'avg_cgroup_memory_usage_in_mib'
+    ]
+
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 120)
+
+    ns = 1000 * 1000 * 1000
+    mio = 1024 * 1024
+    df.loc[:, 'avg_cgroup_memory_limit_in_mib'] = df['avg_cgroup_memory_limit_in_bytes'] / mio
+    df.loc[:, 'avg_cgroup_memory_usage_in_mib'] = df['avg_cgroup_memory_usage_in_bytes'] / mio
+    df.loc[:, 'avg_cpuacct_usage_s'] = df['avg_cpuacct_usage'] / ns
+    df = df[selected_cols]
+
+    df.to_csv('reduced_report.csv', index=False)
     print(df)
-    df.to_csv('report.csv', index=False)
 
     if args.delete_observed:
         collection.drop()
